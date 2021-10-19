f0eaee0581807fb1985f258af4a0fceef7a13faf
tvani1011 <tvani1011@gmail.com>
2021-10-19 15:06:23 +0530

    third commit

---
+++ src/com/ssh/Department.java
@@ -0,0 +1,19 @@
+package com.ssh;
+
+public class Department {
+	private String deptname;
+	private int deptid;
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	public int getDeptid() {
+		return deptid;
+	}
+	public void setDeptid(int deptid) {
+		this.deptid = deptid;
+	}
+
+}
